7e622aa0b077eaad64ced33feddd7bedcd5ae46f
AlperKartkaya <alperkartkaya@gmail.com>
2026-05-10 22:27:38 +0300

    fix: merge conflicts b/w this branch and dev

--- android/app/src/main/java/com/neph/navigation/AppNavGraph.kt
+++ android/app/src/main/java/com/neph/navigation/AppNavGraph.kt
@@ -502,7 +502,10 @@ fun AppNavGraph(
                     navigateToLogin()
                 },
                 onNavigateToMyHelpRequests = {
-                    navigateToDrawerRoute(Routes.MyHelpRequests.route)
+                    val popped = navController.popBackStack(Routes.MyHelpRequests.route, inclusive = false)
+                    if (!popped) {
+                        navigateToDrawerRoute(Routes.MyHelpRequests.route)
+                    }
                 },
                 mobileOnboardingStepId = mobileOnboardingStepId,
                 onMobileOnboardingStepCompleted = onMobileOnboardingStepCompleted,
